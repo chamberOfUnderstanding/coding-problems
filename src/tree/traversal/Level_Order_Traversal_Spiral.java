73b9f0662579d593d388ad46ba753a25c043ba53
47un <59013274+47un@users.noreply.github.com>
2020-12-30 16:30:47 +0530

    Update Level_Order_Traversal_Spiral.java

--- src/tree/traversal/Level_Order_Traversal_Spiral.java
+++ src/tree/traversal/Level_Order_Traversal_Spiral.java
@@ -18,7 +18,6 @@ import java.util.Stack;
  * ======================
  * METHOD 1 : Two stacks
  * ======================
- * Maintain two stacks, one stores the normal order, another the reverse order
  * Iterate until either stack is empty
  *  While the reverse order stack is not empty
  *   Pop out a node and print it
@@ -52,22 +51,41 @@ public class Level_Order_Traversal_Spiral {
     }
 
     private static void spiralLevelOrderTraversal(Node root){
+        // Maintain two stacks, one stores the normal order, another the reverse order
         Stack<Node> reverseOrderStack = new Stack<>();
         Stack<Node> normalOrderStack = new Stack<>();
+        
+        // Root goes into the reverse order
         reverseOrderStack.push(root);
+        
+        // Until either stack is empty
         while(!reverseOrderStack.isEmpty() || !normalOrderStack.isEmpty()){
             Node node = null;
+            
+            // Process the reverse order stack
             while(!reverseOrderStack.isEmpty()){
+                
+                // Pop and process the node
                 node = reverseOrderStack.pop();
                 System.out.print(node.data + " ");
+                
+                // The children of this node lie in the next level and need to be processed in the normal order, which is left and then right.
+                // To achieve this order of processing in a stack, push right BEFORE left
                 if(node.right != null)
                     normalOrderStack.push(node.right);
                 if(node.left != null)
                     normalOrderStack.push(node.left);
             }
+            
+            // Process the normal order stack
             while(!normalOrderStack.isEmpty()){
+                
+                // Pop and process the node
                 node = normalOrderStack.pop();				
                 System.out.print(node.data + " ");
+                
+                // The children of this node lie in the next level and need to be processed in the reverse order, which is right and then left.
+                // To achieve this order of processing in a stack, push left BEFORE right
                 if(node.left != null)
                     normalOrderStack.push(node.left);
                 if(node.right != null)
