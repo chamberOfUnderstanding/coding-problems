2b96ce3173cd4d0a05f172e624b0470740874455
47un <>
2021-03-21 12:37:55 +0530

    2

--- src/dynamic_programming/Maximize_Array_Cost.java
+++ src/dynamic_programming/Maximize_Array_Cost.java
@@ -13,12 +13,15 @@ public class Maximize_Array_Cost {
 		int backup = 0;
 
 		for (int i = 1; i < B.length; i++) {
+			
+			// Cost if previous element is 1
 			int prev_as_1_cost = Math.abs(1 - B[i]);
+			// Cost if current element is 1
 			int curr_as_1_cost = Math.abs(B[i - 1] - 1);                     
 
 			backup = high_c1;
 
-			// Highest cost possible with current as 1
+			// Highest cost with current element as 1
 			high_c1  = Math.max(
 					high_c1, 
 					high_p1 + curr_as_1_cost
