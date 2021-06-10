999773e3b2304e806a56bbfda4f217a090999f79
47un <59013274+47un@users.noreply.github.com>
2021-06-10 19:27:58 +0530

    Update Sort_Three_Itemed_Array.java

--- src/array/Sort_Three_Itemed_Array.java
+++ src/array/Sort_Three_Itemed_Array.java
@@ -13,14 +13,12 @@ package array;
  * =========
  * METHOD 1
  * ===========
- * Use low, mid, high for partitioning
- * Repeat till mid reaches the end (high)
- * If current character is 0 and the one at low is'nt 0, swap it with low
- *   Advance low and mid (coz item[low] is now sorted)
+ * 
+ * 
+ * 
  * If current character is 2 and the one at high is'nt 2, swap it with high
  *   Advance high alone, as the one that just got swapped from high to current, can be a 1.
- *  In that case more 0s may appear after this 1, which need to be sorted to low. So advancing low or mid here breaks the algo
- * If the current character is a 1, advance mid            
+ *              
  *            
  * TIME     : O(n)
  * SPACE    : O(1)
@@ -37,21 +35,30 @@ public class Sort_Three_Itemed_Array {
     }
 
     private static void sortThreeItemedArray(int[] array){
+        // Use low, mid, high for partitioning
         int low  = 0;
         int mid  = 0;
         int high = array.length - 1;
+        
+        // Repeat till mid reaches the end (high)
         while(mid <= high){
             if(array[mid] == 0){
+                // If current character is 0 and the one at low is 1 or 2, swap
                 if(array[low] != 0)
-                    swap(array, mid, low);
+                    swap(array, mid, low);                
+                // advance both as low is considered sorted
                 mid++;
                 low++;
             }
             else if(array[mid] == 2){
+                // If current character is 2 and the one at high is 0 or 1, swap
                 if(array[high] != 2)
                     swap(array, mid, high);
+                // advance only high as the one that just got swapped from high to current, can be a 1
+                // In that case more 0s may appear after this 1, which need to be sorted to low. So advancing low or mid here breaks the algo
                 high--;
             }
+            // If the current character is a 1, advance mid
             else
                 mid++;
         }
